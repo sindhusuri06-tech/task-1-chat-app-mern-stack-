94351eb85f3b90873913a9ec72074ee62659d118
S. Sindhu <sindhusuri06@gmail.com>
2026-02-04 17:52:59 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
